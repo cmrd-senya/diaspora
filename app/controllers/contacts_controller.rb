9ed57a14322fd6c4a6f590a15b4f3134e576d5b2
Steffen van Bergerem <svbergerem@online.de>
2016-08-12 22:10:57 +0200

    Refactor contacts page using pagination

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -16,7 +16,11 @@ class ContactsController < ApplicationController
 
       # Used for mentions in the publisher
       format.json {
-        @people = Person.search(params[:q], current_user, only_contacts: true).limit(15)
+        @people = if params[:q].present?
+                    Person.search(params[:q], current_user, only_contacts: true).limit(15)
+                  else
+                    set_up_contacts_json
+                  end
         render json: @people
       }
     end
@@ -31,29 +35,46 @@ class ContactsController < ApplicationController
 
   def set_up_contacts
     type = params[:set].presence
-    type ||= "by_aspect" if params[:a_id].present?
+    if params[:a_id].present?
+      type ||= "by_aspect"
+      @aspect = current_user.aspects.find(params[:a_id])
+      gon.preloads[:aspect] = AspectPresenter.new(@aspect).as_json
+    end
     type ||= "receiving"
+    @contacts_size = contacts_by_type(type).length
+  end
 
-    @contacts = contacts_by_type(type)
-    @contacts_size = @contacts.length
-    gon.preloads[:contacts] = @contacts.map {|c| ContactPresenter.new(c, current_user).full_hash_with_person }
+  def set_up_contacts_json
+    type = params[:set].presence
+    if params[:a_id].present?
+      type ||= "by_aspect"
+      @aspect = current_user.aspects.find(params[:a_id])
+    end
+    type ||= "receiving"
+    contacts_by_type(type).paginate(page: params[:page], per_page: 25)
+                          .map {|c| ContactPresenter.new(c, current_user).full_hash_with_person }
   end
 
   def contacts_by_type(type)
-    case type
+    order = ["profiles.first_name ASC", "profiles.last_name ASC", "profiles.diaspora_handle ASC"]
+    contacts = case type
       when "all"
+        order.unshift "receiving DESC"
         current_user.contacts
       when "only_sharing"
         current_user.contacts.only_sharing
       when "receiving"
         current_user.contacts.receiving
       when "by_aspect"
-        @aspect = current_user.aspects.find(params[:a_id])
-        gon.preloads[:aspect] = AspectPresenter.new(@aspect).as_json
-        current_user.contacts
+        order.unshift "contact_id IS NOT NULL DESC"
+        current_user.contacts.joins(
+          "LEFT OUTER JOIN aspect_memberships ON contacts.id = contact_id"
+        ).where(aspect_memberships: {aspect_id: [nil, params[:a_id]]})
       else
         raise ArgumentError, "unknown type #{type}"
       end
+    contacts.includes(person: :profile)
+            .order(order)
   end
 
   def set_up_contacts_mobile
